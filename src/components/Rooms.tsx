2139c8017458be5a93e19f93fabc162fe47b7959
Daniel Cox <74686011+lecoxsportif@users.noreply.github.com>
2025-11-08 15:55:26 -0700

    Update Rooms.tsx

--- src/components/Rooms.tsx
+++ src/components/Rooms.tsx
@@ -6,7 +6,7 @@ import gardenRoom from "@/assets/room-garden.jpg";
 const rooms = [{
   id: 1,
   name: "Odopa 1",
-  description: "Odopa means good love in Twi. Odopa 1 is a large room with the best sea, garden and pool views in the Afiki House. This room is on the first floor of the Afiki beach house.",
+  description: "Odopa means good love in Twi. Odopa rooms are large with a four poster king bed. Odopa1 at $87 a night, boasts the best sea, garden and pool views at the Afiki Beach House, plus a balcony overlooking the pool. This room is on the first floor of the Afiki beach house.",
   price: 87,
   capacity: 2,
   image: oceanRoom,
